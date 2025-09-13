75bbffa841aa857df3decdd26c2bd19522a892bf
Sumit <sumitsharma7624@gmail.com>
2025-09-14 03:33:03 +0530

    done changes in ui

--- user-preferences-service/user-preferences-service/src/main/java/com/weatherverse/user_preferences_service/UserPreferencesServiceApplication.java
+++ user-preferences-service/user-preferences-service/src/main/java/com/weatherverse/user_preferences_service/UserPreferencesServiceApplication.java
@@ -1,9 +1,6 @@
 package com.weatherverse.user_preferences_service;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class UserPreferencesServiceApplication {
